dde244f43247099bf40938057199b610b5250841
Kanesh Arujunan <kanesh314@gmail.com>
2017-08-20 12:03:38 +0800

    Add files via upload

---
+++ Wod.js
@@ -0,0 +1,9 @@
+var myName = "asiadevacademy";
+var ourName = "BAC";
+
+document.write("Before Change"+ "<br/>" + myName + "<br/>" + ourName);
+
+myName = myName.toUpperCase();
+ourName = ourName.toLowerCase();
+
+document.write("<br/>" + "After Change"+ "<br/>" + myName + "<br/>" + ourName + "<br/>")
